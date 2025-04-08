6ac8769275182c6fc0dd93336a6ca8bec09e0cac
heojunfo <kleague3@gmail.com>
2025-04-08 14:30:14 +0900

    text fix

--- src/pages/main/components/PWAInstallPrompt.jsx
+++ src/pages/main/components/PWAInstallPrompt.jsx
@@ -146,7 +146,7 @@ const PWAInstallPrompt = ({ onClose }) => {
         <p style={descriptionStyle}>
           {isEnglish
             ? "Add the Dentner app to your home screen for fast and convenient access."
-            : "Dentner앱을 홈 화면에 추가하여<br />빠르고 편리하게 이용하세요."}
+            : "Dentner앱을 홈 화면에 추가하여 빠르고 편리하게 이용하세요."}
         </p>
         
         <button 
